e96741544e34a4460530834fbedcb552357c1717
Clayton Halaska <claytonhalaska@gmail.com>
2017-05-25 11:33:26 -0500

    Add project files.

---
+++ HPNC Website/Controllers/DonationFormSurfaceController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper.Mappers;
+using HPNC_Website.Models;
+using Umbraco.Web.Mvc;
+using Square.Connect.Api;
+using Square.Connect.Client;
+using Square.Connect.Model;
+
+namespace HPNC_Website.Controllers
+{
+    public class DonationFormSurfaceController : SurfaceController
+    {
+        string squareAuthorization = "sq0atp-y3yfJ9z11FzNCI-0yXsT3A";
+        string squareLocationId = "4TF4RMNFM5D9T";
+
+        //string squareLocationId = "CBASEAF9JuhJ7vWfEWKTYiRX57QgAQ"; //sandbox
+        //string squareAuthorization = "sandbox-sq0atb-uIKrJ7mof7lHpc8tBOU1HA"; //sandbox
+
+        public ActionResult RenderDonationForm()
+        {
+            return PartialView("~/Views/Partials/_DonationForm.cshtml", new DonationFormModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleDonationForm(DonationFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return CurrentUmbracoPage();
+            }
+           
+            string key = Guid.NewGuid().ToString();
+            model.Amount = model.Amount * 100;
+            Money money = new Money(model.Amount, Money.CurrencyEnum.USD);
+
+            try
+            {
+                var customerId = CreateCustomer(model);
+                ChargeRequest body = new ChargeRequest(key, money, model.Nonce, null, null, null, null, customerId);
+                TransactionApi transactionApi = new TransactionApi();
+                var response = transactionApi.Charge(squareAuthorization, squareLocationId, body);
+                var confirm = response;
+            }
+            catch (Exception)
+            {
+
+                TempData["DonationError"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            TempData["DonationSuccessful"] = true;
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        public string CreateCustomer(DonationFormModel model)
+        {
+            Address address = new Address(model.StreetAddress1, model.StreetAddress2, null, model.City, null, null, null, model.State, null, null, model.ZipCode, null, model.FirstName, model.LastName, null);
+            CreateCustomerRequest body = new CreateCustomerRequest(model.FirstName, model.LastName, null, null, model.Email, address, model.Phone, null, null);
+            CustomerApi customerAPI = new CustomerApi();
+            var response = customerAPI.CreateCustomer(squareAuthorization, body);
+            return response.Customer.Id;
+        }
+    }
+}
---
+++ HPNC Website/Models/DonationFormModel.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HPNC_Website.Models
+{
+    public class DonationFormModel
+    {
+
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Nonce { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
